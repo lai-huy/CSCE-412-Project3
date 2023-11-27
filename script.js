fca095b4adb628a3902b4d4e58ce51c564761a4f
Huy Lai <lai.huy@tamu.edu>
2023-11-27 14:04:11 -0600

    format html files

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const DEFAULT = "style/default.css";
 const ALT = "style/alternative.css";
 
 function toggleStyleSheet() {
-    var styleElement = document.getElementById("mainStyleSheet");
-    var fileName = styleElement.getAttribute("href");
-    var newFileName = DEFAULT;
-    if (fileName == ALT) {
-        newFileName = DEFAULT
-    } else {
-        newFileName = ALT
-    }
-    styleElement.setAttribute("href", newFileName);
-    localStorage.setItem(STYLESHEET_KEY, newFileName);
+	var styleElement = document.getElementById("mainStyleSheet");
+	var fileName = styleElement.getAttribute("href");
+	var newFileName = DEFAULT;
+	if (fileName == ALT) {
+		newFileName = DEFAULT;
+	} else {
+		newFileName = ALT;
+	}
+	styleElement.setAttribute("href", newFileName);
+	localStorage.setItem(STYLESHEET_KEY, newFileName);
 }
 
 window.onload = function () {
-    var stylesheetValue = localStorage.getItem(STYLESHEET_KEY) || DEFAULT;
-    var styleElement = document.getElementById("mainStyleSheet");
-    styleElement.setAttribute("href", stylesheetValue);
-}
+	var stylesheetValue = localStorage.getItem(STYLESHEET_KEY) || DEFAULT;
+	var styleElement = document.getElementById("mainStyleSheet");
+	styleElement.setAttribute("href", stylesheetValue);
+};
